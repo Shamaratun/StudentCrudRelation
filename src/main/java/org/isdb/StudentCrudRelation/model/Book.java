18077df612c977c954877a58b39e002ddcc0e9f3
Shamaratun <shamaratunmeme@gmail.com>
2025-03-22 22:16:24 +0600

    4 table done

--- src/main/java/org/isdb/StudentCrudRelation/model/Book.java
+++ src/main/java/org/isdb/StudentCrudRelation/model/Book.java
@@ -1,11 +1,12 @@
 package org.isdb.StudentCrudRelation.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,22 +15,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor  
+@NoArgsConstructor
 @Entity(name = "T-Book")
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+	@Column(nullable = false, length = 100)
+	private String name;
 
-    @Column(nullable = false, length = 100)
-    private String author;
+	@Column(nullable = false, length = 100)
+	private String author;
 
-@Column(nullable = false, length = 100)
-    private String publisher;
-    
-   private Class clazz;
+	@Column(nullable = false, length = 100)
+	private String publisher;
 
+	@ManyToOne
+	@JoinColumn(name = "student", referencedColumnName = "id", nullable = false)
+	private Student student;
 }
--- src/main/java/org/isdb/StudentCrudRelation/model/Student.java
+++ src/main/java/org/isdb/StudentCrudRelation/model/Student.java
@@ -2,11 +2,12 @@ package org.isdb.StudentCrudRelation.model;
 
 import java.time.Instant;
 import java.util.List;
-import org.springframework.data.annotation.Id;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -15,46 +16,42 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-
-
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor  
+@NoArgsConstructor
 @Entity(name = "T_Student")
 public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
 
-    @Column(nullable = false, length = 100)
-    private String name;
-    
-    @Column(nullable = false, length = 100)
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @OneToOne
-    @JoinColumn(name = "clazz",referencedColumnName = "id" ,nullable = false)
-    private Class clazz;
+	@Column(nullable = false, length = 100)
+	private String name;
 
-    @Column(nullable = false, length = 30, unique = true)
-    private Integer roll;
+	@Column(nullable = false, length = 100)
+	private String email;
 
-    @OneToMany(mappedBy = "student")
-    private List<Book> book;
+	@OneToOne
+	@JoinColumn(name = "student_class", referencedColumnName = "id", nullable = false)
+	private Class studentClass;
 
-    @Column(nullable = false, length = 100)
-    private String phone; 
+	@Column(nullable = false, unique = true)
+	private int roll;
 
-     @Column(length = 100)
-    private String address;
+	@OneToMany(mappedBy = "student")
+	private List<Book> books; // Renamed to plural for clarity
 
-    @Column(nullable = false, length = 100)
-    private String gender;
+	@Column(nullable = false, length = 100)
+	private String phone;
 
-    @Column(nullable = false, length = 100)
-    private Instant dob;
+	@Column(length = 100)
+	private String address;
 
+	@Column(nullable = false, length = 100)
+	private String gender;
 
+	@Column(nullable = false)
+	private Instant dob;
 }
